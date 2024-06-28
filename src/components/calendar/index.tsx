430196fedb4a8c5fd9388098d7b755bc98d20fd0
abura1han <xbura1han@gmail.com>
2024-06-28 10:34:42 +0600

    add calendar day view

--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -13,7 +13,7 @@ import {
 import { animated, useSpring } from "@react-spring/web";
 import { Loader2 } from "lucide-react";
 import moment from "moment";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 const UlkaCalendar = ({
   containerClassName,
@@ -209,8 +209,8 @@ const CalWeekView = ({ onClick }: { onClick: () => void }) => {
                   <div
                     key={time}
                     className="w-full h-full flex justify-center relative border-b hover:bg-indigo-50"
+                    onClick={onClick}
                   >
-
                     {/* Time indicator */}
                     {checkIsToday(day.date) && (
                       <CalTimeIndicator slotTime={time} />
@@ -231,15 +231,125 @@ const CalWeekView = ({ onClick }: { onClick: () => void }) => {
 };
 
 const CalDayView = ({ onClick }: { onClick: () => void }) => {
-  const currentMonth = moment().month() + 1;
-  const months = getMonths()[currentMonth];
+  const currentYear = moment().year();
+  const currentMonth = moment().month() + 1; // moment().month() returns 0-based month index
+  const currentDate = moment().date();
+  const currentWeek = getCurrentWeek(currentYear, currentMonth, currentDate);
+  const times = getTimesWithAmPm();
+
+  const today = useMemo(() => {
+    return currentWeek.filter((day) => checkIsToday(day.date));
+  }, [currentWeek]);
 
   return (
-    <DayBlockInMonthView name={months} month={currentMonth} onClick={onClick} />
+    <div className="flex h-[90vh]">
+      <div className="mt-[74px] text-xs mr-2 flex flex-col justify-between">
+        {/* Time rows */}
+        {times.map((time) => (
+          <div key={time} className="">
+            {time}
+          </div>
+        ))}
+        <div key={"last-item"}></div>
+      </div>
+      {/* Calendar view */}
+      <div className="flex-1 grid grid-cols-1 text-sm text-blue-800 font-medium text-center">
+        {today?.map((day, index) => (
+          <div key={day.date + day.dayName} className="flex flex-col">
+            {/* Day name in week loop */}
+            <div>{day.dayName.slice(0, 3)}</div>
+            <div
+              className={cn(
+                "flex flex-1 flex-col justify-start mt-2 border-r relative",
+                index === 0 && "border-l"
+              )}
+            >
+              {/* Date in week loop */}
+              <div className="border-b w-full h-max flex justify-center pb-3">
+                <span
+                  className={cn(
+                    "size-8 rounded-full bg-gray-100 flex items-center justify-center",
+                    checkIsToday(day.date) && "bg-blue-200 font-bold"
+                  )}
+                >
+                  {day.date.split("-").at(-1)}
+                </span>
+              </div>
+
+              {/* Time slots in day loop */}
+              <div className="flex-1 hover:bg-gray-50 text-xs flex flex-col justify-between">
+                {times.map((time) => (
+                  <div
+                    key={time}
+                    className="w-full h-full flex justify-center relative border-b hover:bg-indigo-50"
+                  >
+                    {/* Time indicator */}
+                    {checkIsToday(day.date) && (
+                      <CalTimeIndicator slotTime={time} />
+                    )}
+                  </div>
+                ))}
+                <div
+                  key={"last-slot"}
+                  className="w-full flex justify-center"
+                ></div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
 const DayBlockInMonthView = ({
+  name,
+  month,
+  onClick,
+}: {
+  name: string;
+  month: number;
+  onClick: () => void;
+}) => {
+  const currentYear = moment().year();
+  const currentMonth = moment().month() + 1;
+  const weekDays = getWeekDayNames();
+  const days = getDaysByYearAndMonth(currentYear, currentMonth);
+
+  return (
+    <div>
+      <div className="mt-4">
+        <div className="grid grid-cols-7 text-sm text-blue-800 font-medium text-center">
+          {weekDays?.map((day) => (
+            <div key={day}>{day.slice(0, 2)}</div>
+          ))}
+        </div>
+        {false ? (
+          <div className="h-[170px] flex items-center justify-center">
+            <Loader2 className="animate-spin mx-auto" />
+          </div>
+        ) : (
+          <div className="grid grid-cols-7 mt-10 h-[84vh] justify-center text-center">
+            {days?.map((day) => (
+              <div
+                key={day.date}
+                className={cn(
+                  `${day.active ? "text-black" : "text-gray-400"}`,
+                  "mt-2 text-sm h-28 hover:bg-gray-100 w-full"
+                )}
+                onClick={onClick}
+              >
+                {day.date}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+const DayBlockInDayView = ({
   name,
   month,
   onClick,
@@ -309,7 +419,6 @@ const CalTimeIndicator = ({ slotTime }: { slotTime: string }) => {
         currentFormattedTime === slotTime.replace(slotTime.slice(2, 5), "")
       );
 
-      console.log({ slot: slotTime.replace(slotTime.slice(2, 5), '') });
     }, 500);
 
     setLoading(false);
