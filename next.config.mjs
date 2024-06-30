16a509633298024dd7ee2a8880f7998cc2347475
abura1han <xbura1han@gmail.com>
2024-06-30 08:34:08 +0600

    add react compiler

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     remotePatterns: [{ protocol: "https", hostname: "images.pexels.com" }],
   },
+  experimental: {
+    reactCompiler: true,
+  },
 };
 
 export default nextConfig;
